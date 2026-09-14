5e9d3e8936d757720b6562240ea3a405519fc9df
Mizanur Rahman <mizan.rahman@zhiltd.com>
2026-09-14 12:12:52 +0300

    sdfs

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   app.enableShutdownHooks(); // graceful shutdown for ECS
 
   app.enableCors({
-    origin: 'http://localhost:5173', // Vite's default dev server port
+    origin: 'http://localhost:5173', // Vite's default dev server port sdf
     credentials: true,
   });
 
